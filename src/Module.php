ef0eb8ed73e56e6b631bfbeaeaf1498179c4a33d
ivankristic <thechosen.os@gmail.com>
2017-12-13 16:00:54 +0100

    authFix - added `RequestLogger`, `Registration` and `ConfirmRegistration` listeners in config - added listeners, model adapters, primary model adapters, services, resources,   model fields, acl rules and authenticatable models registrations to `Module`

--- src/Module.php
+++ src/Module.php
@@ -3,6 +3,8 @@
 namespace Framework\CrudApi;
 
 use Framework\Base\Module\BaseModule;
+use Framework\CrudApi\Repository\GenericRepository;
+use Framework\RestApi\RestApiConfigurationInterface;
 
 /**
  * Class Module
@@ -19,16 +21,110 @@ class Module extends BaseModule
         $configDirPath = realpath(dirname(__DIR__)) . '/config/';
         $this->setModuleConfiguration($configDirPath);
 
+        /**
+         * @var CrudApiApplicationInterface $application
+         */
         $application = $this->getApplication();
+        /**
+         * @var RestApiConfigurationInterface $appConfig
+         */
         $appConfig = $application->getConfiguration();
+        $repositoryManager = $application->getRepositoryManager();
 
         // Add routes to dispatcher
         $application->getDispatcher()
                     ->addRoutes($appConfig->getPathValue('routes'));
 
-        // Register resources, repositories and model fields
-        $this->getApplication()
-             ->getRepositoryManager()
-             ->registerRepositories($appConfig->getPathValue('repositories'));
+        // Add listeners to application
+        if (
+            empty($listeners = $appConfig->getPathValue('listeners')) === false
+        ) {
+            foreach ($listeners as $event => $arrayHandlers) {
+                foreach ($arrayHandlers as $handlerClass) {
+                    $application->listen($event, $handlerClass);
+                }
+            }
+        }
+
+        // Register model adapters
+        if (
+            empty($modelAdapters = $appConfig->getPathValue('modelAdapters')) === false
+        ) {
+            foreach ($modelAdapters as $model => $adapters) {
+                foreach ($adapters as $adapter) {
+                    $repositoryManager->addModelAdapter($model, new $adapter());
+                }
+            }
+        }
+
+        // Register model primary adapters
+        if (
+            empty($primaryModelAdapter = $appConfig->getPathValue('primaryModelAdapter')) === false
+        ) {
+            foreach ($primaryModelAdapter as $model => $primaryAdapter) {
+                $repositoryManager->setPrimaryAdapter($model, new $primaryAdapter());
+            }
+        }
+
+        //Register Services
+        if (
+            empty($services = $appConfig->getPathValue('services')) === false
+        ) {
+            foreach ($services as $serviceName => $config) {
+                $application->registerService(new $serviceName($config));
+            }
+        }
+
+        if (
+            empty($models = $appConfig->getPathValue('models')) === false
+        ) {
+            // Format models configuration
+            $modelsConfiguration = $this->generateModelsConfiguration($models);
+
+            // Register resources and model fields
+            $repositoryManager->registerResources($modelsConfiguration['resources'])
+                              ->registerModelFields($modelsConfiguration['modelFields']);
+        }
+
+        // Register repositories
+        if (
+            empty($repositories = $appConfig->getPathValue('repositories')) === false
+        ) {
+            $repositoryManager->registerRepositories($repositories);
+        }
+
+        // Register Acl rules
+        if (
+            empty($acl = $appConfig->getPathValue('acl')) === false
+        ) {
+            $application->setAclRules($acl);
+        }
+
+        // Register authenticatable models
+        if (
+            empty($auth = $appConfig->getAuthenticatables()) === false
+        ) {
+            $application->getRepositoryManager()
+                        ->addAuthenticatableModels($auth);
+        }
+    }
+
+    /**
+     * @param $modelsConfig
+     *
+     * @return array
+     */
+    private function generateModelsConfiguration(array $modelsConfig)
+    {
+        $generatedConfiguration = [
+            'resources' => [],
+            'modelFields' => [],
+        ];
+        foreach ($modelsConfig as $modelName => $options) {
+            $generatedConfiguration['resources'][$options['collection']] = GenericRepository::class;
+            $generatedConfiguration['modelFields'][$options['collection']] = $options['fields'];
+        }
+
+        return $generatedConfiguration;
     }
 }
