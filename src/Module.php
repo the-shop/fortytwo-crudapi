12766eb721931c63f06a9afb70141c5ff62eb5f4
ivankristic <thechosen.os@gmail.com>
2017-12-19 17:01:43 +0100

    Update - composer dependency updated - accommodated changes from config refactor - `Module` now only registers model fields and resources

--- src/Module.php
+++ src/Module.php
@@ -15,97 +15,33 @@ class Module extends BaseModule
     /**
      * @inheritdoc
      */
-    public function bootstrap()
+    public function loadConfig()
     {
         // Let's read all files from module config folder and set to Configuration
         $configDirPath = realpath(dirname(__DIR__)) . '/config/';
         $this->setModuleConfiguration($configDirPath);
+    }
 
-        /**
-         * @var CrudApiApplicationInterface $application
-         */
-        $application = $this->getApplication();
-        /**
-         * @var RestApiConfigurationInterface $appConfig
-         */
-        $appConfig = $application->getConfiguration();
-        $repositoryManager = $application->getRepositoryManager();
-
-        // Add routes to dispatcher
-        $application->getDispatcher()
-                    ->addRoutes($appConfig->getPathValue('routes'));
-
-        // Add listeners to application
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap()
+    {
         if (
-            empty($listeners = $appConfig->getPathValue('listeners')) === false
-        ) {
-            foreach ($listeners as $event => $arrayHandlers) {
-                foreach ($arrayHandlers as $handlerClass) {
-                    $application->listen($event, $handlerClass);
-                }
-            }
-        }
-
-        // Register model adapters
-        if (
-            empty($modelAdapters = $appConfig->getPathValue('modelAdapters')) === false
-        ) {
-            foreach ($modelAdapters as $model => $adapters) {
-                foreach ($adapters as $adapter) {
-                    $repositoryManager->addModelAdapter($model, new $adapter());
-                }
-            }
-        }
-
-        // Register model primary adapters
-        if (
-            empty($primaryModelAdapter = $appConfig->getPathValue('primaryModelAdapter')) === false
-        ) {
-            foreach ($primaryModelAdapter as $model => $primaryAdapter) {
-                $repositoryManager->setPrimaryAdapter($model, new $primaryAdapter());
-            }
-        }
-
-        //Register Services
-        if (
-            empty($services = $appConfig->getPathValue('services')) === false
-        ) {
-            foreach ($services as $serviceName => $config) {
-                $application->registerService(new $serviceName($config));
-            }
-        }
-
-        if (
-            empty($models = $appConfig->getPathValue('models')) === false
+            empty(
+                $models = $this->getApplication()
+                               ->getConfiguration()
+                               ->getPathValue('models')
+            ) === false
         ) {
             // Format models configuration
             $modelsConfiguration = $this->generateModelsConfiguration($models);
 
             // Register resources and model fields
-            $repositoryManager->registerResources($modelsConfiguration['resources'])
-                              ->registerModelFields($modelsConfiguration['modelFields']);
-        }
-
-        // Register repositories
-        if (
-            empty($repositories = $appConfig->getPathValue('repositories')) === false
-        ) {
-            $repositoryManager->registerRepositories($repositories);
-        }
-
-        // Register Acl rules
-        if (
-            empty($acl = $appConfig->getPathValue('acl')) === false
-        ) {
-            $application->setAclRules($acl);
-        }
-
-        // Register authenticatable models
-        if (
-            empty($auth = $appConfig->getAuthenticatables()) === false
-        ) {
-            $application->getRepositoryManager()
-                        ->addAuthenticatableModels($auth);
+            $this->getApplication()
+                 ->getRepositoryManager()
+                 ->registerResources($modelsConfiguration['resources'])
+                 ->registerModelFields($modelsConfiguration['modelFields']);
         }
     }
 
